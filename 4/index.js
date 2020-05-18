7a8531b75c0b7b0bb33d41463c22210e664273b7
Adam Nagy <an2208@nyu.edu>
2020-05-18 16:17:55 +0200

    added input box for name

--- 4/index.js
+++ 4/index.js
@@ -12,34 +12,32 @@ app.use(
   })
 );
 
-let coordinatesDatabase = new Datastore('coordinatesDatabase.db');
+const coordinatesDatabase = new Datastore("coordinatesDatabase.db");
 coordinatesDatabase.loadDatabase();
 
 app.post("/api", (request, response) => {
   console.log(request.body);
   const data = request.body;
-  response.json({
-    status: "success",
-    latitude: data.latitude,
-    longitude: data.longitude,
-  });
-  
-  const timestamp = Date.now()
+  const timestamp = Date.now();
   data.date = new Date(Date.now());
   data.timestamp = timestamp;
-  coordinatesDatabase.push(data);
-  
+  coordinatesDatabase.insert(data);
+  response.json({
+    status: "success",
+    username: data.username,
+    latitude: data.latitude,
+    longitude: data.longitude,
+    date: data.date,
+  });
+
   const logInfo = `${data.latitude},${data.longitude},${data.date}\n`;
   fs.appendFile("records/records.csv", logInfo, (err) => {
     if (err) return console.error(err);
   });
-  
-  
+
   const dateString = data.date.toDateString();
   const logText = `latitude: ${data.latitude}, longitude: ${data.longitude}, date: ${dateString}\n`;
   fs.appendFile("records/records.txt", logText, (err) => {
     if (err) return console.error(err);
   });
 });
-
-
